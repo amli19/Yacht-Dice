64880a71086b0ad850bba89c13dd4a21696e32d1
Amy <ali19@mail.sfsu.edu>
2021-05-10 16:41:16 -0700

    Creating Dice rolls. Initial layout.

--- Yahtzee/YahtzeeViewController.swift
+++ Yahtzee/YahtzeeViewController.swift
@@ -14,18 +14,34 @@ class YahtzeeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+
     
     @IBOutlet weak var D1: UIImageView!
+    
     @IBOutlet weak var D2: UIImageView!
+    
     @IBOutlet weak var D3: UIImageView!
+    
     @IBOutlet weak var D4: UIImageView!
+    
     @IBOutlet weak var D5: UIImageView!
     
     
-    @IBAction func RollButton(_ sender: Any) {
+    @IBAction func Roll(_ sender: Any) {
+        //rolls random number from 1-6
+        let D1Roll = arc4random_uniform(6) + 1
+        let D2Roll = arc4random_uniform(6) + 1
+        let D3Roll = arc4random_uniform(6) + 1
+        let D4Roll = arc4random_uniform(6) + 1
+        let D5Roll = arc4random_uniform(6) + 1
+            
+        //displays rolls
+        D1.image = UIImage(named: "Dice\(D1Roll)")
+        D2.image = UIImage(named: "Dice\(D2Roll)")
+        D2.image = UIImage(named: "Dice\(D3Roll)")
+        D2.image = UIImage(named: "Dice\(D4Roll)")
+        D2.image = UIImage(named: "Dice\(D5Roll)")
     }
-    
-    
     
 }
 
