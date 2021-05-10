1acfc3da1cc35a5b4e597953cafa74a377c98162
Amy <ali19@mail.sfsu.edu>
2021-05-10 06:24:27 -0700

    initial commit

---
+++ Yahtzee/YahtzeeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Yahtzee
+//
+//  Created by JPL-ST-SPRING2021 on 5/10/21.
+//
+
+import UIKit
+
+class YahtzeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBOutlet weak var D1: UIImageView!
+    @IBOutlet weak var D2: UIImageView!
+    @IBOutlet weak var D3: UIImageView!
+    @IBOutlet weak var D4: UIImageView!
+    @IBOutlet weak var D5: UIImageView!
+    
+    
+    @IBAction func RollButton(_ sender: Any) {
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
