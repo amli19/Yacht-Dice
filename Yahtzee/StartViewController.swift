ae6837efdc0deece4feedeea0f1278925100277c
Amy <ali19@mail.sfsu.edu>
2021-05-10 20:55:23 -0700

    players being implemented

--- Yahtzee/StartViewController.swift
+++ Yahtzee/StartViewController.swift
@@ -12,7 +12,7 @@ class StartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBOutlet weak var TitleCard: UIImageView!
--- Yahtzee/YahtzeeViewController.swift
+++ Yahtzee/YahtzeeViewController.swift
@@ -17,23 +17,23 @@ class YahtzeeViewController: UIViewController {
 
     var diceRoll:[Dice]=[]
     var count = 3
-    let D1Item = Dice(value: 1, selected: false)
-    let D2Item = Dice(value: 1, selected: false)
-    let D3Item = Dice(value: 1, selected: false)
-    let D4Item = Dice(value: 1, selected: false)
-    let D5Item = Dice(value: 1, selected: false)
+    var Player = 1
+    var D1Item = Dice(value: 1, selected: false)
+    var D2Item = Dice(value: 1, selected: false)
+    var D3Item = Dice(value: 1, selected: false)
+    var D4Item = Dice(value: 1, selected: false)
+    var D5Item = Dice(value: 1, selected: false)
     
 
     @IBOutlet weak var NumberOfRolls: UILabel!
     
     @IBOutlet weak var D1: UIButton!
-    
     @IBOutlet weak var D2: UIButton!
     @IBOutlet weak var D3: UIButton!
-    
     @IBOutlet weak var D4: UIButton!
-    
     @IBOutlet weak var D5: UIButton!
+    
+    @IBOutlet weak var currentPlayer: UILabel!
     
     @IBAction func Roll(_ sender: Any) {
         //rolls random number from 1-6 for all dices not selected
@@ -93,21 +93,48 @@ class YahtzeeViewController: UIViewController {
         diceRoll.append(D3Item)
         diceRoll.append(D4Item)
         diceRoll.append(D5Item)
+        self.currentPlayer.text = "Player 1 Turn"
         self.NumberOfRolls.text="Rolls: "+String(count)
     }
     
-    //
-//    func DiceArray{
-//    }
-//    func endOfTurn{
-//      reset #rolls to 3
-//      switch current player to false and next one to true
-//    }
-
+//move to next player
+    func nextPlayer(){
+        count = 3
+        if(Player == 1){
+            self.currentPlayer.text = "Player 2 Turn"
+            Player = 2
+            nextTurnReset()
+        }
+        if(Player == 2){
+            self.currentPlayer.text = "Player 1 Turn"
+            Player = 1
+            nextTurnReset()
+        }
+    }
+//reset when player ends turn
+    func nextTurnReset() {
+        D1Item.selected=false
+        D2Item.selected=false
+        D3Item.selected=false
+        D4Item.selected=false
+        D5Item.selected=false
+        
+        D1Item.value=1
+        D2Item.value=1
+        D3Item.value=1
+        D4Item.value=1
+        D5Item.value=1
+        
+        D1.setImage(UIImage(named: "Dice1"), for: .normal)
+        D2.setImage(UIImage(named: "Dice1"), for: .normal)
+        D3.setImage(UIImage(named: "Dice1"), for: .normal)
+        D4.setImage(UIImage(named: "Dice1"), for: .normal)
+        D5.setImage(UIImage(named: "Dice1"), for: .normal)
+    }
+    
     struct Dice {
         var value:Int
         var selected:Bool
-        
     }
     
 }
